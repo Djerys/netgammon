c313ec12aff5889a6d223aafe130ed9fc6fae954
Dmitry <40293610+Djerys@users.noreply.github.com>
2020-04-04 20:33:46 +0300

    Add new files

--- test.py
+++ test.py
@@ -1,12 +1,39 @@
-from logic import Roll
-from logic import Board
+from dataclasses import dataclass
+from base import system
 
 
-board = Board()
-roll = Roll()
-moves = board.possible_moves(roll, 13)
-print(roll)
-print(moves)
-print(board)
-board.move(13, moves[0])
-print(board)
+@dataclass
+class ComponentA(system.Component):
+    a: int = 1
+
+
+@dataclass
+class ComponentB(system.Component):
+    b: int = 2
+
+
+@dataclass
+class ComponentC(system.Component):
+    c: int = 3
+
+
+@system.requires(ComponentA, ComponentB)
+class SomeSystem(system.System):
+    def update(self):
+        for entity in self.required_entities:
+            component_a = entity.get_component(ComponentA)
+            component_a.a += 1
+            component_b = entity.get_component(ComponentB)
+            component_b.b += 1
+
+
+world = system.World()
+e1 = world.create_entity(ComponentA(), ComponentB())
+e2 = world.create_entity(ComponentA(), ComponentC())
+e3 = world.create_entity(ComponentB(), ComponentC())
+e4 = world.create_entity(ComponentC())
+some_system = SomeSystem()
+world.add_system(some_system)
+
+while True:
+    world.update()
