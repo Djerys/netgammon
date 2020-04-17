cf1097a7014c7892571df3e326e04366b4107385
Dmitry <40293610+Djerys@users.noreply.github.com>
2020-04-17 21:47:14 +0300

    Add new files

--- backgammon_game/config.py
+++ backgammon_game/config.py
@@ -7,55 +7,55 @@ FRAME_RATE = 30
 
 CAPTION = 'Backgammon'
 
-BACKGROUND_IMAGE = 'backgammon_game/assets/image/background.png'
+BACKGROUND_IMAGE = 'assets/image/background.png'
 
-WHITE_PIECE_IMAGE = 'backgammon_game/assets/image/white_piece.png'
-RED_PIECE_IMAGE = 'backgammon_game/assets/image/red_piece.png'
+WHITE_PIECE_IMAGE = 'assets/image/white_piece.png'
+RED_PIECE_IMAGE = 'assets/image/red_piece.png'
 
-WHITE_FROM_IMAGE = 'backgammon_game/assets/image/white_from_point.png'
-RED_FROM_IMAGE = 'backgammon_game/assets/image/red_from_point.png'
+WHITE_FROM_IMAGE = 'assets/image/white_from_point.png'
+RED_FROM_IMAGE = 'assets/image/red_from_point.png'
 
-TO_IMAGE = 'backgammon_game/assets/image/to_point.png'
+TO_IMAGE = 'assets/image/to_point.png'
 
-HISTORY_FILENAME = 'backgammon_game/backgammon_history.txt'
+HISTORY_FILENAME = 'backgammon_history.txt'
 
 VISIBLE_NUMBER = 7
 
 DIE_IMAGES = {
     color.WHITE: {
-        1: 'backgammon_game/assets/image/white_dice_1.png',
-        2: 'backgammon_game/assets/image/white_dice_2.png',
-        3: 'backgammon_game/assets/image/white_dice_3.png',
-        4: 'backgammon_game/assets/image/white_dice_4.png',
-        5: 'backgammon_game/assets/image/white_dice_5.png',
-        6: 'backgammon_game/assets/image/white_dice_6.png'
+        1: 'assets/image/white_dice_1.png',
+        2: 'assets/image/white_dice_2.png',
+        3: 'assets/image/white_dice_3.png',
+        4: 'assets/image/white_dice_4.png',
+        5: 'assets/image/white_dice_5.png',
+        6: 'assets/image/white_dice_6.png'
     },
     color.RED: {
-        1: 'backgammon_game/assets/image/red_dice_1.png',
-        2: 'backgammon_game/assets/image/red_dice_2.png',
-        3: 'backgammon_game/assets/image/red_dice_3.png',
-        4: 'backgammon_game/assets/image/red_dice_4.png',
-        5: 'backgammon_game/assets/image/red_dice_5.png',
-        6: 'backgammon_game/assets/image/red_dice_6.png'
+        1: 'assets/image/red_dice_1.png',
+        2: 'assets/image/red_dice_2.png',
+        3: 'assets/image/red_dice_3.png',
+        4: 'assets/image/red_dice_4.png',
+        5: 'assets/image/red_dice_5.png',
+        6: 'assets/image/red_dice_6.png'
     }
 }
 
 BANNER_IMAGES = {
-    1: 'backgammon_game/assets/image/banner1.png',
-    2: 'backgammon_game/assets/image/banner2.png',
-    3: 'backgammon_game/assets/image/banner3.png',
-    4: 'backgammon_game/assets/image/banner4.png',
-    5: 'backgammon_game/assets/image/banner5.png',
-    6: 'backgammon_game/assets/image/banner6.png',
-    7: 'backgammon_game/assets/image/banner7.png',
-    8: 'backgammon_game/assets/image/banner8.png',
+    1: 'assets/image/banner1.png',
+    2: 'assets/image/banner2.png',
+    3: 'assets/image/banner3.png',
+    4: 'assets/image/banner4.png',
+    5: 'assets/image/banner5.png',
+    6: 'assets/image/banner6.png',
+    7: 'assets/image/banner7.png',
+    8: 'assets/image/banner8.png',
 }
 
 MENU_BUTTON_IMAGES = {
-    graphic.LOCAL: 'backgammon_game/assets/image/local_pvp.png',
+    graphic.LOCAL: 'assets/image/local_pvp.png',
     graphic.WIN: {
-        color.RED: 'backgammon_game/assets/image/red_wins.png',
-        color.WHITE: 'backgammon_game/assets/image/white_wins.png'
+        color.RED: 'assets/image/red_wins.png',
+        color.WHITE: 'assets/image/white_wins.png'
     },
-    graphic.NET: 'backgammon_game/assets/image/net_pvp.png'
+    graphic.NET: 'assets/image/net_pvp.png'
 }
--- backgammon_server/server.py
+++ backgammon_server/server.py
@@ -5,12 +5,33 @@ import socketserver
 HOST, PORT = 'localhost', 4567
 
 
+class PlayersPair:
+    _next_pair = None
+
+    _selection_lock = threading.Lock()
+
+    def __init__(self):
+        self.current_player = None
+
+    @classmethod
+    def join(cls, player):
+        with cls._selection_lock:
+            if cls._next_pair is None:
+                cls._next_pair = PlayersPair()
+                player.pair = cls._next_pair
+                player.color = 'W'
+            else:
+                player.color = 'R'
+                player.pair = cls._next_pair
+                cls._next_pair = None
+
+
 class ThreadingTCPServer(socketserver.ThreadingMixIn, socketserver.TCPServer):
     daemon_threads = True
     allow_reuse_address = True
 
 
-class BackgammonHandler(socketserver.StreamRequestHandler):
+class PlayerHandler(socketserver.StreamRequestHandler):
     def handle(self):
         client = f'{self.client_address} on {threading.current_thread().name}'
         print(f'Connected: {client}')
@@ -22,6 +43,6 @@ class BackgammonHandler(socketserver.StreamRequestHandler):
             print(f'Received on {threading.current_thread().name}: {data}')
 
 
-with ThreadingTCPServer((HOST, PORT), BackgammonHandler) as server:
+with ThreadingTCPServer((HOST, PORT), PlayerHandler) as server:
     print('Backgammon server is running')
     server.serve_forever()
